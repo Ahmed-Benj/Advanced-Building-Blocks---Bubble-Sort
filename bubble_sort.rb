4242beb5309e4f0d45e5def712a80a128b4aba97
Ahmed-Benj <ahmedblaugrana@gmail.com>
2020-12-13 23:41:03 +0100

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,20 @@
-
 def bubble_sort(array)
-  index= 0
-  number_of_times_sort=0
+  index = 0
+  number_of_times_sort = 0
 
-  while(number_of_times_sort<array.length)
-    while (index<array.length-1)
-      if(array[index]>array[index+1])
-        substitute_value=array[index]
-        array[index]=array[index+1]
-        array[index+1]=substitute_value
+  while number_of_times_sort < array.length
+    while index < array.length - 1
+      if array[index] > array[index + 1]
+        substitute_value = array[index]
+        array[index] = array[index + 1]
+        array[index + 1] = substitute_value
       end
-      index+=1
+      index += 1
     end
-    number_of_times_sort +=1
-    index=0
+    number_of_times_sort += 1
+    index = 0
   end
   print array
 end
 
-
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,23 +1,20 @@
-
 bubble_sort_by(array) do |left, right|
+  index = 0
+  number_of_times_sort = 0
 
-  index= 0
-  number_of_times_sort=0
-
-  while(number_of_times_sort<array.length)
-    while (index<array.length-1)
-      if(left.length - right.length)
-        substitute_value=left
-        left=right
-        right=substitute_value
+  while number_of_times_sort < array.length
+    while index < array.length - 1
+      if left.length - right.length
+        substitute_value = left
+        left = right
+        right = substitute_value
       end
-      index+=1
+      index += 1
     end
-    number_of_times_sort +=1
-    index=0
+    number_of_times_sort += 1
+    index = 0
   end
   print array
 end
 
-
-bubble_sort_b(["hi","hello","hey"])
+bubble_sort_b(%w[hi hello hey])
