cc8b9203dfdb4575efa3bae6c601d0d071264691
Ahmed-Benj <ahmedblaugrana@gmail.com>
2020-12-15 00:06:36 +0100

    fix linters

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,13 +1,13 @@
-def bubble_sort_by(array) 
+def bubble_sort_by(array)
   index = 0
   number_of_times_sort = 0
 
   while number_of_times_sort < array.length
     while index < array.length - 1
-      if (yield array[index], array[index+1]).to_i >= 0
+      if (yield array[index], array[index + 1]).to_i >= 0
         substitute_value = array[index]
-        array[index] = array[index+1]
-        array[index+1] = substitute_value
+        array[index] = array[index + 1]
+        array[index + 1] = substitute_value
       end
       index += 1
     end
@@ -15,8 +15,4 @@ def bubble_sort_by(array)
     index = 0
   end
   return array
-end
-
-bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
-  left.length - right.length
 end
